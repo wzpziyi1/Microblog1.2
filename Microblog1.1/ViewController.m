54810c4bcb9b6f3b878470af83f75899fb59f718
wzpziyi <wzpziyi@gmail.com>
2015-03-20 19:51:18 +0800

    Initial Commit

---
+++ Microblog1.1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Microblog1.1
+//
+//  Created by 王志盼 on 15/3/20.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Microblog1.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Microblog1.1
+//
+//  Created by 王志盼 on 15/3/20.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
